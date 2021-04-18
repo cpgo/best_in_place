77bd7342479db2c8f36980f7640f15d68c22cff5
Cassio Godinho <cassiopgodinho@gmail.com>
2021-04-18 19:00:40 -0300

    add context param to initializer

--- lib/best_in_place/railtie.rb
+++ lib/best_in_place/railtie.rb
@@ -4,7 +4,8 @@ require 'action_view/base'
 module BestInPlace
   class Railtie < ::Rails::Railtie #:nodoc:
     config.after_initialize do
-      BestInPlace::ViewHelpers = ActionView::Base.new({}, {}, "")
+      context = ActionView::LookupContext.new(ActionController::Base.view_paths)
+      BestInPlace::ViewHelpers = ActionView::Base.new(context, {}, '')
     end
   end
 end
